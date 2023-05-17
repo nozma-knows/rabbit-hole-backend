37d114cd0e19fb5c1c6ce3d90052a6bcd48d099e
Noah Milberger <n.milberger@gmail.com>
2023-05-17 15:11:04 -0400

    Revert changes.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { readFileSync } from "fs";
 import { resolvers } from "./graph/resolvers";
 import { expressjwt } from "express-jwt";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors";
 
 const typeDefs = readFileSync("./src/graph/schema.graphql", {
   encoding: "utf-8",
@@ -13,21 +12,13 @@ const typeDefs = readFileSync("./src/graph/schema.graphql", {
 
 const startServer = async () => {
   const app = express();
-  // app.use(
-  //   expressjwt({
-  //     secret: `${process.env.JWT_PRIVATE_KEY}`,
-  //     algorithms: ["HS256"],
-  //     credentialsRequired: false,
-  //   })
-  // );
   app.use(
-    "/api",
-    cors<cors.CorsRequest>({
-      origin: ["http://localhost:3000", "https://rabbit-hole-pi.vercel.app"],
-      credentials: true,
+    expressjwt({
+      secret: `${process.env.JWT_PRIVATE_KEY}`,
+      algorithms: ["HS256"],
+      credentialsRequired: false,
     })
   );
-
   const httpServer = createServer(app);
 
   const prisma = new PrismaClient();
@@ -37,9 +28,9 @@ const startServer = async () => {
     resolvers,
     context: async ({ req, res }) => ({
       prisma, // prisma client
-      // userId: req.headers.userId, // user id from token
-      // expiry: req.headers.expiry, // expiry from token
-      // token: req.headers.authorization?.split("Bearer ")[1], // token
+      userId: req.headers.userId, // user id from token
+      expiry: req.headers.expiry, // expiry from token
+      token: req.headers.authorization?.split("Bearer ")[1], // token
     }),
   });
 
@@ -56,45 +47,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-// import { createServer } from "http";
-// import express from "express";
-// import { ApolloServer } from "apollo-server-express";
-// import { readFileSync } from "fs";
-// import { resolvers } from "./graph/resolvers";
-// // import { expressjwt } from "express-jwt";
-// import { PrismaClient } from "@prisma/client";
-// import cors from "cors";
-// import { json } from "body-parser";
-
-// const typeDefs = readFileSync("./src/graph/schema.graphql", {
-//   encoding: "utf-8",
-// });
-
-// const startServer = async () => {
-//   const app = express();
-//   const httpServer = createServer(app);
-//   const prisma = new PrismaClient();
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: async ({ req, res }) => ({
-//       prisma, // prisma client
-//     }),
-//   });
-//   await server.start();
-//   app.use(
-//     "/api",
-//     cors<cors.CorsRequest>({
-//       origin: ["https://rabbit-hole-pi.vercel.app", "http://localhost:3000"],
-//     }),
-//     json()
-//   );
-
-//   await new Promise<void>((resolve) =>
-//     httpServer.listen({ port: 4000 }, resolve)
-//   );
-//   console.log(`🚀 Server ready at http://localhost:4000/api`);
-// };
-
-// startServer();
