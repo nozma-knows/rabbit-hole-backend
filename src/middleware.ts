234140ecfc6e4b6025a09d0604d3f52f42d92805
Noah Milberger <n.milberger@gmail.com>
2023-05-18 11:10:37 -0400

    CORS.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
+import cors from "cors";
 import { expressjwt } from "express-jwt";
 
 module.exports = [
-  function (req: any, res: any, next: any) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  },
+  cors({
+    credentials: true,
+  }),
   expressjwt({
     secret: `${process.env.JWT_PRIVATE_KEY}`,
     algorithms: ["HS256"],
