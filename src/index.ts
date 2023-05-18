a5098035d90374a87b9c28db3d04ce0d0cf181d4
Noah Milberger <n.milberger@gmail.com>
2023-05-18 09:35:50 -0400

    Fixed bug with server.

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,3 @@
-// import { createServer } from "http";
-// import express from "express";
-// import { ApolloServer } from "apollo-server-express";
-// import { readFileSync } from "fs";
-// import { resolvers } from "./graph/resolvers";
-// import { expressjwt } from "express-jwt";
-// import { PrismaClient } from "@prisma/client";
-// import cors from "cors";
-
-// const typeDefs = readFileSync("./src/graph/schema.graphql", {
-//   encoding: "utf-8",
-// });
-
-// const startServer = async () => {
-//   const app = express();
-//   app.use(
-//     cors({ origin: "*" }),
-//     expressjwt({
-//       secret: `${process.env.JWT_PRIVATE_KEY}`,
-//       algorithms: ["HS256"],
-//       credentialsRequired: false,
-//     })
-//   );
-//   const httpServer = createServer(app);
-
-//   const prisma = new PrismaClient();
-
-//   const apolloServer = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     context: async ({ req, res }) => ({
-//       prisma, // prisma client
-//       userId: req.headers.userId, // user id from token
-//       expiry: req.headers.expiry, // expiry from token
-//       token: req.headers.authorization?.split("Bearer ")[1], // token
-//     }),
-//   });
-
-//   await apolloServer.start();
-
-//   apolloServer.applyMiddleware({
-//     app,
-//     path: "/api",
-//   });
-
-//   httpServer.listen({ port: process.env.PORT || 4000 }, () =>
-//     console.log(`Server listening on localhost:4000${apolloServer.graphqlPath}`)
-//   );
-// };
-
-// startServer();
-
 import { createServer } from "http";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -57,7 +5,6 @@ import { readFileSync } from "fs";
 import { resolvers } from "./graph/resolvers";
 import { expressjwt } from "express-jwt";
 import { PrismaClient } from "@prisma/client";
-import cors from "cors";
 
 const typeDefs = readFileSync("./src/graph/schema.graphql", {
   encoding: "utf-8",
@@ -65,19 +12,15 @@ const typeDefs = readFileSync("./src/graph/schema.graphql", {
 
 const startServer = async () => {
   const app = express();
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    cors({
-      origin: ["http://localhost:3000", "https://rabbit-hole-pi.vercel.app"],
-    });
+  app.use(
     expressjwt({
       secret: `${process.env.JWT_PRIVATE_KEY}`,
       algorithms: ["HS256"],
       credentialsRequired: false,
-    });
-  });
-
+    })
+  );
   const httpServer = createServer(app);
+
   const prisma = new PrismaClient();
 
   const apolloServer = new ApolloServer({
@@ -85,6 +28,9 @@ const startServer = async () => {
     resolvers,
     context: async ({ req, res }) => ({
       prisma, // prisma client
+      userId: req.headers.userId, // user id from token
+      expiry: req.headers.expiry, // expiry from token
+      token: req.headers.authorization?.split("Bearer ")[1], // token
     }),
   });
 
